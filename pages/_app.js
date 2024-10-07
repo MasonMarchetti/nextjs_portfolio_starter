cea8878cfb5ece729c948bbd056eeafd52dc5fb0
MasonMarchetti <masonmarchetti24@gmail.com>
2024-10-07 00:25:01 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,33 @@
-import 'nextra-theme-blog/style.css'
-import Head from 'next/head'
-
-import '../styles/main.css'
+import 'nextra-theme-blog/style.css';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import '../styles/main.css'; // Assuming you have custom styles here
 
 export default function Nextra({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <>
+      {/* Conditionally render the name and image only on the home page */}
+      {router.pathname === '/' && (
+        <div className="header-container">
+          {/* Centered Name */}
+          <h1 className="centered-name">Mason T. Marchetti</h1>
+
+          {/* Circular Image */}
+          <div className="header-image">
+            <Image
+              src="/images/photo3.jpg"
+              alt="Header Image"
+              width={200}
+              height={200}  // Ensure width and height are equal for the circular image
+              style={{ borderRadius: '50%' }}
+            />
+          </div>
+        </div>
+      )}
+
       <Head>
         <link
           rel="alternate"
@@ -21,7 +43,7 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} />  {/* Render the current page */}
     </>
-  )
+  );
 }
--- pages/posts/index.md
+++ pages/posts/index.md
@@ -1,6 +1,6 @@
 ---
-type: posts
-title: Posts
+type: page
+title: Experience
 date: 2021-03-18
 ---
 
